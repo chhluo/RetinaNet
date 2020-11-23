a88744112cda8a6dfed41b62a75d792a31d5c86e
luochunhua <luochunhua1996@outlook.com>
2020-11-23 09:34:23 -0500

    first commit

---
+++ labelEncoder.py
@@ -0,0 +1,85 @@
+"""
+    ## Encoding labels
+
+    The raw labels, consisting of bounding boxes and class ids need to be
+    transformed into targets for training. This transformation consists of
+    the following steps:
+
+    - Generating anchor boxes for the given image dimensions
+    - Assigning ground truth boxes to the anchor boxes
+    - The anchor boxes that are not assigned any objects, are either assigned the
+    background class or ignored depending on the IOU
+    - Generating the classification and regression targets using anchor boxes
+"""
+import tensorflow as tf
+from anchor import AnchorBox
+from utillity import *
+
+class LabelEncoder:
+    """Transforms the raw labels into targets for training.
+
+    This class has operations to generate targets for a batch of samples which
+    is made up of the input images, bounding boxes for the objects present and
+    their class ids.
+
+    Attributes:
+      anchor_box: Anchor box generator to encode the bounding boxes.
+      box_variance: The scaling factors used to scale the bounding box targets.
+    """
+
+    def __init__(self):
+        self._anchor_box = AnchorBox()
+        self._box_variance = tf.convert_to_tensor(
+            [0.1, 0.1, 0.2, 0.2], dtype=tf.float32
+        )
+
+    def _match_anchor_boxes(
+        self, anchor_boxes, gt_boxes, match_iou=0.5, ignore_iou=0.4
+    ):
+        """Matches ground truth boxes to anchor boxes based on IOU.
+
+        1. Calculates the pairwise IOU for the M `anchor_boxes` and N `gt_boxes`
+          to get a `(M, N)` shaped matrix.
+        2. The ground truth box with the maximum IOU in each row is assigned to
+          the anchor box provided the IOU is greater than `match_iou`.
+        3. If the maximum IOU in a row is less than `ignore_iou`, the anchor
+          box is assigned with the background class.
+        4. The remaining anchor boxes that do not have any class assigned are
+          ignored during training.
+
+        Arguments:
+          anchor_boxes: A float tensor with the shape `(total_anchors, 4)`
+            representing all the anchor boxes for a given input image shape,
+            where each anchor box is of the format `[x, y, width, height]`.
+          gt_boxes: A float tensor with shape `(num_objects, 4)` representing
+            the ground truth boxes, where each box is of the format
+            `[x, y, width, height]`.
+          match_iou: A float value representing the minimum IOU threshold for
+            determining if a ground truth box can be assigned to an anchor box.
+          ignore_iou: A float value representing the IOU threshold under which
+            an anchor box is assigned to the background class.
+
+        Returns:
+          matched_gt_idx: Index of the matched object
+          positive_mask: A mask for anchor boxes that have been assigned ground
+            truth boxes.
+          ignore_mask: A mask for anchor boxes that need to by ignored during
+            training
+        """
+
+        pass
+
+    def _compute_box_target(self, anchor_boxes, matched_gt_boxes):
+        """Transforms the ground truth boxes into targets for training"""
+
+        pass
+
+    def _encode_sample(self, image_shape, gt_boxes, cls_ids):
+        """Creates box and classification targets for a single sample"""
+
+        pass
+
+    def encode_batch(self, batch_images, gt_boxes, cls_ids):
+        """Creates box and classification targets for a batch"""
+
+        pass
