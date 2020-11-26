0dacb3713f3bf19b8bfe4b5fc448eaeced6bcc52
luochunhua <luochunhua1996@outlook.com>
2020-11-26 10:34:08 +0000

    add loss

--- loss.py
+++ loss.py
@@ -45,3 +45,42 @@ class RetinaNetClassificationLoss(tf.losses.Loss):
         pt = tf.where(tf.equal(y_true, 1.0), probs, 1 - probs)
         loss = alpha * tf.pow(1.0 - pt, self._gamma) * cross_entropy
         return tf.reduce_sum(loss, axis=-1)
+
+
+class RetinaNetLoss(tf.losses.Loss):
+
+    def __init__(self, num_classes=80, alpha=0.25, gamma=2.0, delta=1.0):
+        super(RetinaNetLoss, self).__init__(
+            reduction="auto", name="RetinaNetLoss")
+        self._cls_loss = RetinaNetClassificationLoss(alpha, gamma)
+        self._box_loss = RetinaNetBoxLoss(delta)
+        self._num_classes = num_classes
+
+    def call(self, y_true, y_pred):
+        y_pred = tf.cast(y_pred, dtype=tf.float32)
+        box_labels = y_true[..., :4]
+        box_predictions = y_pred[..., :4]
+
+        cls_labels = tf.one_hot(
+            tf.cast(y_true[..., 4], dtype=tf.int32),
+            self._num_classes,
+            dtype=tf.float32
+        )
+        cls_predictions = y_pred[..., 4:]
+
+        positive_mask = tf.cast(tf.greater(
+            y_true[..., 4], -1.0), dtype=tf.float32)
+
+        ignore_mask = tf.cast(tf.equal(y_true[..., 4], -2.0), dtype=tf.float32)
+        cls_loss = self._cls_loss(cls_labels, cls_predictions)
+        box_loss = self._box_loss(box_labels, box_predictions)
+        cls_loss = tf.where(tf.equal(ignore_mask, 1.0), 0.0, cls_loss)
+        box_loss = tf.where(tf.equal(positive_mask, 1.0), box_loss, 0.0)
+
+        normalizer = tf.reduce_sum(positive_mask, axis=-1)
+        cls_loss = tf.math.divide_no_nan(
+            tf.reduce_sum(cls_loss, axis=-1), normalizer)
+        box_loss = tf.math.divide_no_nan(
+            tf.reduce_sum(box_loss, axis=-1), normalizer)
+        loss = cls_loss + box_loss
+        return loss
